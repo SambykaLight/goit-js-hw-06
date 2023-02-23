546ba497270bc8f100d88ca685fe49da1e34393c
Nataliia D <113416916+SambykaLight@users.noreply.github.com>
2023-02-23 09:11:20 +0100

    final

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 let controlSlider = document.getElementById("font-size-control");
 
 let text = document.getElementById("text");
-
+text.style.fontSize = controlSlider.value + "px";
 controlSlider.oninput = function() {
   text.style.fontSize = controlSlider.value + "px";
 };
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const raibow = document.querySelector('.color');
+const changeRainbowBtn = document.querySelector('.change-color');
+const body = document.body;
+
+
+const changeRainbow = () =>{
+  raibow.textContent = getRandomHexColor();
+  body.style.backgroundColor = raibow.textContent;
+};
+
+changeRainbowBtn.addEventListener('click', changeRainbow);
